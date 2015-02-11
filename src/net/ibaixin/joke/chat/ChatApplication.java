2c328113b7a8bf4c34bd4241f0c165a132486665
huanghui6579 <huanghui6579@163.com>
2015-02-11 17:41:58 +0800

    1、修改包名 2、修改聊天界面

---
+++ src/net/ibaixin/joke/chat/ChatApplication.java
@@ -0,0 +1,358 @@
+package net.ibaixin.joke.chat;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import net.ibaixin.joke.chat.model.Emoji;
+import net.ibaixin.joke.chat.model.EmojiType;
+import net.ibaixin.joke.chat.model.Personal;
+import net.ibaixin.joke.chat.model.SystemConfig;
+import net.ibaixin.joke.chat.service.CoreService;
+import net.ibaixin.joke.chat.util.Constants;
+import net.ibaixin.joke.chat.util.SystemUtil;
+import net.ibaixin.joke.chat.util.XmppConnectionManager;
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.graphics.Bitmap;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+
+/**
+ * 应用程序入口
+ * @author huanghui1
+ *
+ */
+public class ChatApplication extends Application {
+	private LinkedList<Activity> activities = new LinkedList<>();
+	
+	private static SystemConfig systemConfig;
+	
+	private static ChatApplication instance;
+	
+	private SharedPreferences preferences;
+	
+	/**
+	 * 当前的用户
+	 */
+	private static Personal currentUser = null;
+	/**
+	 * 当前用户的账号
+	 */
+	private static String currentAccount = null;
+	
+	/**
+	 * 经典表情集合
+	 */
+	private static Map<String, Emoji> mEmojiMap = null;
+	private static List<Emoji> mEmojis = null;
+	/**
+	 * 表情类型集合
+	 */
+	private static List<EmojiType> mEmojiTypes = null;
+	public static int emojiTypeCount = 0;
+	
+	public static int emojiPageCount = 0;
+	
+	/**
+	 * 每页显示的表情数量，不含删除按钮
+	 */
+	private static final int PAGE_SIZE = 20;
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		instance = this;
+		preferences = getSharedPreferences(Constants.SETTTING_LOGIN, Context.MODE_PRIVATE);
+		systemConfig = new SystemConfig();
+		currentUser = new Personal();
+		
+		// 在使用 SDK 各组间之前初始化 context 信息，传入 ApplicationContext
+		SDKInitializer.initialize(this);
+		
+		initSystemConfig();
+		
+		initImageLoaderConfig();
+
+		initEmojiType();
+		
+		initEmoji();
+		
+	}
+
+	public String getCurrentAccount() {
+		return currentAccount;
+	}
+
+	public void setCurrentAccount(String currentAccount) {
+		ChatApplication.currentAccount = currentAccount;
+	}
+
+	/**
+	 * 初始化表情
+	 * @update 2014年10月27日 上午11:20:53
+	 */
+	private void initEmoji() {
+		if (mEmojiMap == null) {
+			mEmojiMap = new HashMap<>();
+		} else {
+			mEmojiMap.clear();
+		}
+		if (mEmojis == null) {
+			mEmojis = new ArrayList<>();
+		} else {
+			mEmojis.clear();
+		}
+		List<String> list = SystemUtil.getEmojiFromFile("emoji");
+		//表情格式为“f_static_000,[微笑]”
+		if (list != null && list.size() > 0) {
+			for (String str : list) {
+				String[] arr = str.split(",");
+				String faceName = arr[0];
+				String description = arr[1];
+				int resId = SystemUtil.getRespurceIdByName(faceName);
+				if (resId > 0) {
+					Emoji emoji = new Emoji();
+					emoji.setResId(resId);
+					emoji.setFaceName(faceName);
+					emoji.setDescription(description);
+					mEmojis.add(emoji);
+					mEmojiMap.put(description, emoji);
+				}
+			}
+		}
+		int emojiSize = mEmojis.size();
+		if (emojiSize > 0) {
+			//向上取整：Math.ceil(1.4)=2.0 
+			emojiPageCount = (int) Math.ceil(emojiSize / 20 + 0.1);
+		}
+	}
+	
+	/**
+	 * 初始化表情的类型
+	 * @update 2014年10月27日 下午7:59:06
+	 */
+	private void initEmojiType() {
+		if (mEmojiTypes == null) {
+			mEmojiTypes = new ArrayList<>();
+		} else {
+			mEmojiTypes.clear();
+		}
+		//assets文件内容格式：emotionstore_emoji_icon,经典表情,1,最后一个字段是表情的操作类型，分为“显示表情”、“管理本地表情”、“添加表情”
+		List<String> list = SystemUtil.getEmojiFromFile("emojiType");
+		if (list != null && list.size() > 0) {
+			for (String str : list) {
+				String[] arr = str.split(",");
+				String fileName = arr[0];
+				String description = arr[1];
+				int optType = Integer.parseInt(arr[2]);
+				int resId = SystemUtil.getRespurceIdByName(fileName);
+				if (resId > 0) {
+					EmojiType emojiType = new EmojiType();
+					emojiType.setResId(resId);
+					emojiType.setFileName(fileName);
+					emojiType.setDescription(description);
+					emojiType.setOptType(optType);
+					mEmojiTypes.add(emojiType);
+				}
+			}
+			emojiTypeCount = mEmojiTypes.size();
+		}
+	}
+	
+	/**
+	 * 获得所有的表情集合
+	 * @update 2014年10月27日 下午3:02:25
+	 * @return
+	 */
+	public static List<Emoji> getEmojis() {
+		return mEmojis;
+	}
+	
+	/**
+	 * 获得所有的表情集合
+	 * @update 2014年10月27日 下午3:02:25
+	 * @return
+	 */
+	public static Map<String, Emoji> getEmojiMap() {
+		return mEmojiMap;
+	}
+	
+	public static List<EmojiType> geEmojiTypes() {
+		return mEmojiTypes;
+	}
+	
+	/**
+	 * 根据当前页面的索引获得当前页面的所有表情
+	 * @update 2014年10月27日 下午3:03:10
+	 * @param position
+	 * @return
+	 */
+	public static List<Emoji> getCurrentPageEmojis(int position) {
+		int startIndex = position * PAGE_SIZE;
+		int endIndex = startIndex + PAGE_SIZE;
+		int emojiSize = mEmojiMap.size();
+		if (endIndex > emojiSize) {
+			endIndex = emojiSize;
+		}
+		List<Emoji> subList = new ArrayList<>();
+		subList.addAll(mEmojis.subList(startIndex, endIndex));
+		if (subList.size() < PAGE_SIZE) {	//最后一页不足20个时，就补充空的占位置
+			for (int i = subList.size(); i < PAGE_SIZE; i++) {
+				Emoji emoji = new Emoji();
+				emoji.setResTpe(Emoji.TYPE_EMPTY);
+				subList.add(emoji);
+			}
+		}
+		if (subList.size() == PAGE_SIZE) {
+			Emoji emoji = new Emoji();
+			emoji.setResId(R.drawable.chat_emoji_del_selector);
+			emoji.setResTpe(Emoji.TYPE_DEL);
+			subList.add(emoji);
+		}
+		return subList;
+	}
+
+	/**
+	 * 配置图片加载的工具
+	 * @update 2014年10月24日 上午11:08:16
+	 */
+	private void initImageLoaderConfig() {
+		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
+					.memoryCacheExtraOptions(480, 800)
+			        .diskCacheExtraOptions(480, 800, null)
+			        .denyCacheImageMultipleSizesInMemory()	//同一个imageUri只允许在内存中有一个缓存的bitmap
+			        .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
+			        .memoryCacheSize(2 * 1024 * 1024)
+			        .diskCacheSize(50 * 1024 * 1024)
+			        .defaultDisplayImageOptions(getDefaultDisplayOptions())
+			        .writeDebugLogs()
+			        .build();
+		ImageLoader.getInstance().init(config);
+	}
+	
+	/**
+	 * 获取图片加载默认的图片显示配置
+	 * @update 2014年10月24日 上午11:17:14
+	 * @return
+	 */
+	private DisplayImageOptions getDefaultDisplayOptions() {
+		DisplayImageOptions options = new DisplayImageOptions.Builder()
+				.showImageOnLoading(R.drawable.ic_stub)
+				.showImageForEmptyUri(R.drawable.ic_empty)
+				.showImageOnFail(R.drawable.ic_error)
+				.cacheInMemory(true)
+				.cacheOnDisk(true)
+				.imageScaleType(ImageScaleType.IN_SAMPLE_INT)
+				.bitmapConfig(Bitmap.Config.RGB_565)	//防止内存溢出
+				.displayer(new FadeInBitmapDisplayer(200))
+				.build();
+		return options;
+	}
+	
+	/**
+	 * 初始化系统配置
+	 * @update 2014年10月9日 上午8:17:20
+	 */
+	private void initSystemConfig() {
+		systemConfig.setAccount(preferences.getString(Constants.USER_ACCOUNT, null));
+		systemConfig.setPassword(preferences.getString(Constants.USER_PASSWORD, null));
+		systemConfig.setFirstLogin(preferences.getBoolean(Constants.USER_ISFIRST, true));
+//		systemConfig.setResource(preferences.getString(Constants.USER_RESOURCE, SystemUtil.getPhoneModel()));
+//		systemConfig.setHost(preferences.getString(Constants.NAME_SERVER_HOST, Constants.SERVER_HOST));
+//		systemConfig.setPort(preferences.getInt(Constants.NAME_SERVER_PORT, Constants.SERVER_PORT));
+//		systemConfig.setServerName(preferences.getString(Constants.NAME_SERVER_NAME, Constants.SERVER_NAME));
+	}
+	
+	/**
+	 * 保存系统配置信息
+	 * @update 2014年10月9日 上午8:22:30
+	 * @param config
+	 */
+	public void saveSystemConfig() {
+		Editor editor = preferences.edit();
+		editor.putString(Constants.USER_ACCOUNT, systemConfig.getAccount());
+		editor.putString(Constants.USER_PASSWORD, systemConfig.getPassword());
+		editor.putBoolean(Constants.USER_ISFIRST, systemConfig.isFirstLogin());
+//		editor.putString(Constants.USER_RESOURCE, systemConfig.getResource());
+//		editor.putString(Constants.NAME_SERVER_HOST, systemConfig.getHost());
+//		editor.putString(Constants.NAME_SERVER_NAME, systemConfig.getServerName());
+//		editor.putInt(Constants.NAME_SERVER_PORT, systemConfig.getPort());
+		editor.commit();
+	}
+	
+	/**
+	 * systemconfig
+	 * @update 2014年10月9日 上午8:17:49
+	 * @return
+	 */
+	public SystemConfig getSystemConfig() {
+		return systemConfig;
+	}
+	
+	public Personal getCurrentUser() {
+		return currentUser;
+	}
+	
+	/**
+	 * 获得全局的application
+	 * @return 全局的application
+	 */
+	public static ChatApplication getInstance() {
+		return instance;
+	}
+	
+	/**
+	 * 添加Activity的队列中，用于软件的退出
+	 * @update 2014年10月8日 下午10:22:30
+	 * @param activity
+	 */
+	public void addActivity(Activity activity) {
+		activities.add(activity);
+	}
+	
+	/**
+	 * 退出应用应用程序
+	 * @update 2014年10月8日 下午10:30:04
+	 */
+	public void exit() {
+		XmppConnectionManager.getInstance().disconnect();
+		Intent intent = new Intent(instance, CoreService.class);
+		stopService(intent);
+		for(Activity activity : activities) {
+			activity.finish();
+		}
+		System.exit(0);
+	}
+	
+	/**
+	 * 将Activity从队列中移除
+	 * @update 2014年10月8日 下午10:23:15
+	 * @param activity
+	 */
+	public void removeActivity(Activity activity) {
+		activities.remove(activity);
+	}
+	
+	/**
+	 * 检查该好友是否是自己
+	 * @update 2014年10月24日 下午5:21:33
+	 * @param username
+	 * @return
+	 */
+	public boolean isSelf(String username) {
+		return username.equals(currentUser.getUsername());
+	}
+}
---
+++ src/net/ibaixin/joke/chat/activity/LocationShareActivity.java
@@ -0,0 +1,482 @@
+package net.ibaixin.joke.chat.activity;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.ibaixin.joke.chat.R;
+import net.ibaixin.joke.chat.model.LocationInfo;
+import net.ibaixin.joke.chat.util.SystemUtil;
+import net.ibaixin.joke.chat.view.ProgressWheel;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.Color;
+import android.os.Environment;
+import android.os.Handler;
+import android.support.v4.view.MenuItemCompat;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.CheckedTextView;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.InfoWindow;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.Marker;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.map.SupportMapFragment;
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.search.core.PoiInfo;
+import com.baidu.mapapi.search.core.SearchResult;
+import com.baidu.mapapi.search.geocode.GeoCodeResult;
+import com.baidu.mapapi.search.geocode.GeoCoder;
+import com.baidu.mapapi.search.geocode.OnGetGeoCoderResultListener;
+import com.baidu.mapapi.search.geocode.ReverseGeoCodeOption;
+import com.baidu.mapapi.search.geocode.ReverseGeoCodeResult;
+
+/**
+ * 发送地理位置的界面
+ * @author huanghui1
+ * @version 1.0.0
+ * @update 2015年1月5日 下午9:32:22
+ */
+public class LocationShareActivity extends BaseActivity implements OnGetGeoCoderResultListener, 
+	BaiduMap.OnMapLoadedCallback, BaiduMap.SnapshotReadyCallback {
+	
+	/**
+	 * 刷新菜单按钮的消息
+	 */
+	private static final int MSG_REFRESH_MENU = 0x1;
+	
+	private BaiduMap mBaiduMap;
+
+	// UI相关
+	boolean isFirstLoc = true;// 是否首次定位
+	
+	/**
+	 * 我的 位置的经纬度
+	 */
+	private LatLng myLatLng;
+	
+	/**
+	 * 地图支持的fragment
+	 */
+	private SupportMapFragment mMapFragment;
+	private ImageButton btnMyLocation;
+	private GeoCoder mSearch = null; // 搜索模块，也可去掉地图模块独立使用
+	// 定位相关
+	private LocationClient mLocClient;
+	public MyLocationListenner myListener = new MyLocationListenner();
+	private List<LocationInfo> mLocationInfos = new ArrayList<>();
+	private InfoWindow mInfoWindow;
+	private Marker mMarker;
+	private BitmapDescriptor mCurrentBitmap;
+	
+	/**
+	 * 我的地理位置
+	 */
+	private BDLocation mLocation;
+
+	private LocationAdapter mAdapter;
+	private Handler mHandler = new Handler() {
+		@Override
+		public void handleMessage(android.os.Message msg) {
+			switch (msg.what) {
+			case MSG_REFRESH_MENU:	//刷新菜单按钮
+				if (btnOpt != null && !btnOpt.isEnabled()) {
+					btnOpt.setEnabled(true);
+				}
+				break;
+
+			default:
+				break;
+			}
+		}
+	};
+	
+	private ListView lvData;
+	
+	private ProgressWheel pbLoading;
+	
+	/**
+	 * 显示popup信息的view
+	 */
+	private TextView mPopuInfoView;
+	
+	/**
+	 * 当前的地图是否加载完毕，只有当地图加载完毕后才可定位，否则会抛异常
+	 */
+	private boolean isMapLoaded = false;
+
+	private TextView btnOpt;
+
+	/**
+	 * 当前选中的位置
+	 */
+	private int mCurrentPosition;
+
+	@Override
+	protected int getContentView() {
+		return R.layout.activity_location_share;
+	}
+
+	@Override
+	protected void initView() {
+		if (mMapFragment == null) {
+			mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+		}
+		lvData = (ListView) findViewById(R.id.lv_data);
+		pbLoading = (ProgressWheel) findViewById(R.id.pb_loading);
+		btnMyLocation = (ImageButton) findViewById(R.id.btn_my_location);
+	}
+
+	@Override
+	protected void initData() {
+		//初始化百度地图
+		setUpMap();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater menuInflater = getMenuInflater();
+		menuInflater.inflate(R.menu.common_opt, menu);
+		MenuItem menuDone = menu.findItem(R.id.action_select_complete);
+		btnOpt = (TextView) MenuItemCompat.getActionView(menuDone);
+		btnOpt.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				mBaiduMap.snapshot(LocationShareActivity.this);
+//				final List<PhotoItem> selects = mPhotoAdapter.getSelectList();
+//				pDialog = ProgressDialog.show(mContext, null, getString(R.string.chat_sending_file), false, true);
+//				//发送图片
+//				SystemUtil.getCachedThreadPool().execute(new Runnable() {
+//					
+//					@Override
+//					public void run() {
+//						final ArrayList<MsgInfo> msgList = msgManager.getMsgInfoListByPhotos(msgInfo, selects, false);
+//						Message msg = mHandler.obtainMessage();
+//						if (!SystemUtil.isEmpty(msgList)) {	//消息集合
+//							msg.what = Constants.MSG_SUCCESS;
+//							msg.obj = msgList;
+//						} else {
+//							msg.what = Constants.MSG_FAILED;
+//						}
+//						mHandler.sendMessage(msg);
+//					}
+//				});
+				
+			}
+		});
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	/**
+	 * 定位SDK监听函数
+	 */
+	public class MyLocationListenner implements BDLocationListener {
+
+		@Override
+		public void onReceiveLocation(final BDLocation location) {
+			// map view 销毁后不在处理新接收的位置
+			drawLocation(location);
+		}
+	}
+	
+	/**
+	 * 绘制地理位置信息
+	 * @update 2015年2月7日 下午3:34:00
+	 * @param location
+	 */
+	private void drawLocation(final BDLocation location) {
+		if (!isMapLoaded) {
+			mHandler.postDelayed(new Runnable() {
+				
+				@Override
+				public void run() {
+					drawLocation(location);
+				}
+			}, 500);
+		} else {
+			if (location == null || mBaiduMap == null) {
+				return;
+			}
+			MyLocationData locData = new MyLocationData.Builder()
+					.accuracy(location.getRadius())
+					// 此处设置开发者获取到的方向信息，顺时针0-360
+					.direction(100).latitude(location.getLatitude())
+					.longitude(location.getLongitude()).build();
+			mBaiduMap.setMyLocationData(locData);
+			if (isFirstLoc) {
+				mHandler.sendEmptyMessage(MSG_REFRESH_MENU);	//刷新菜单按钮
+				isFirstLoc = false;
+				mLocation = location;
+				LatLng ll = new LatLng(location.getLatitude(),
+						location.getLongitude());
+				myLatLng = ll;
+				MapStatusUpdate u = MapStatusUpdateFactory.newLatLng(ll);
+				mBaiduMap.animateMapStatus(u);
+				// 反Geo搜索
+				mSearch.reverseGeoCode(new ReverseGeoCodeOption()
+						.location(ll));
+			}
+		}
+	}
+	
+	/**
+	 * 设置map对象的相关参数
+	 * @update 2015年1月5日 下午9:59:30
+	 */
+	private void setUpMap() {
+		if (mBaiduMap == null) {
+			mBaiduMap = mMapFragment.getBaiduMap();
+		}
+		
+		MapView mapView = mMapFragment.getMapView();
+		//去掉缩放控件
+		mapView.showZoomControls(false);
+
+		// 初始化搜索模块，注册事件监听
+		mSearch = GeoCoder.newInstance();
+		mSearch.setOnGetGeoCodeResultListener(this);
+		
+		//地图加载完毕的监听器
+		mBaiduMap.setOnMapLoadedCallback(this);
+		
+		// 开启定位图层
+		mBaiduMap.setMyLocationEnabled(true);
+		mBaiduMap.setMapStatus(MapStatusUpdateFactory.zoomTo(18));
+		
+		// 定位初始化
+		mLocClient = new LocationClient(this);
+		mLocClient.registerLocationListener(myListener);
+		//设置定位选项
+		LocationClientOption option = new LocationClientOption();
+		option.setOpenGps(true);// 打开gps
+		option.setCoorType("bd09ll"); // 设置坐标类型
+		option.setScanSpan(2000);	//定位的间隔时间,单位毫秒
+		option.setIsNeedAddress(true);
+		mLocClient.setLocOption(option);
+		mLocClient.start();
+	}
+
+	@Override
+	protected void addListener() {
+		btnMyLocation.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				MapStatusUpdate u = MapStatusUpdateFactory.newLatLng(myLatLng);
+				mBaiduMap.animateMapStatus(u);
+			}
+		});
+		
+		mBaiduMap.setOnMyLocationClickListener(new BaiduMap.OnMyLocationClickListener() {
+			
+			@Override
+			public boolean onMyLocationClick() {
+				if (mLocation != null) {
+					if (mInfoWindow != null) {
+						mBaiduMap.hideInfoWindow();
+					}
+					showInfoWindow("[我的位置]\r\n" + mLocation.getAddrStr(), myLatLng);
+					return true;
+				}
+				return false;
+			}
+		});
+		
+		mBaiduMap.setOnMarkerClickListener(new BaiduMap.OnMarkerClickListener() {
+			
+			@Override
+			public boolean onMarkerClick(Marker marker) {
+				showInfoWindow(marker.getTitle(), marker.getPosition());
+				return true;
+			}
+		});
+		lvData.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				if (position != 0 && position != mCurrentPosition) {
+					mCurrentPosition = position;
+					LocationInfo locationInfo = mLocationInfos.get(position);
+					LatLng location = new LatLng(locationInfo.getLatitude(), locationInfo.getLongitude());
+					if (mCurrentBitmap == null) {
+						mCurrentBitmap = BitmapDescriptorFactory.fromResource(R.drawable.ic_markers);
+					}
+					OverlayOptions ooA = new MarkerOptions().position(location).icon(mCurrentBitmap)
+							.zIndex(9);
+					mBaiduMap.clear();
+					mMarker = (Marker) mBaiduMap.addOverlay(ooA);
+					mMarker.setTitle(locationInfo.getAddress());
+					mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newLatLng(location));
+				}
+			}
+		});
+	}
+	
+	/**
+	 * 显示popupwindow信息
+	 * @update 2015年2月7日 下午5:04:36
+	 * @param msg
+	 * @param latLng
+	 */
+	private void showInfoWindow(String msg, LatLng latLng) {
+		if (mPopuInfoView == null) {
+			mPopuInfoView = new TextView(getApplicationContext());
+			mPopuInfoView.setBackgroundResource(R.drawable.location_popu_info_bg_normal);
+			mPopuInfoView.setTextColor(Color.WHITE);
+			mPopuInfoView.setText(msg);
+		}
+		mInfoWindow = new InfoWindow(BitmapDescriptorFactory.fromView(mPopuInfoView), latLng, -40, new InfoWindow.OnInfoWindowClickListener() {
+			
+			@Override
+			public void onInfoWindowClick() {
+				// TODO Auto-generated method stub
+				mBaiduMap.hideInfoWindow();
+			}
+		});
+		mBaiduMap.showInfoWindow(mInfoWindow);
+	}
+
+	@Override
+	public void onMapLoaded() {
+		isMapLoaded = true;
+	}
+	
+	@Override
+	protected void onDestroy() {
+		if (mCurrentBitmap != null) {
+			mCurrentBitmap.recycle();
+		}
+		// 退出时销毁定位
+		mLocClient.stop();
+		// 关闭定位图层
+		mBaiduMap.setMyLocationEnabled(false);
+		super.onDestroy();
+	}
+
+	@Override
+	public void onGetGeoCodeResult(GeoCodeResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onGetReverseGeoCodeResult(ReverseGeoCodeResult result) {
+		if (result != null && result.error == SearchResult.ERRORNO.NO_ERROR) {
+			if (mLocationInfos.size() > 0) {
+				mLocationInfos.clear();
+			}
+			LatLng location = result.getLocation();
+			LocationInfo info = new LocationInfo(location.latitude, location.longitude, "[位置]" + result.getAddress());
+			mLocationInfos.add(info);
+			List<PoiInfo> pois = result.getPoiList();
+			if (pois != null && pois.size() > 0) {
+				for (PoiInfo poiInfo : pois) {
+					location = poiInfo.location;
+					info = new LocationInfo(location.latitude, location.longitude, poiInfo.address + poiInfo.name);
+					mLocationInfos.add(info);
+				}
+			}
+			if (mAdapter == null) {
+				mAdapter = new LocationAdapter(mLocationInfos, getApplicationContext());
+				lvData.setAdapter(mAdapter);
+				lvData.setItemChecked(0, true);	//第一位默认选中
+			} else {
+				mAdapter.notifyDataSetChanged();
+			}
+			if (pbLoading.getVisibility() == View.VISIBLE) {
+				pbLoading.setVisibility(View.GONE);
+			}
+		}
+	}
+	
+	/**
+	 * 位置适配器
+	 * @author huanghui1
+	 * @update 2015年1月6日 上午10:06:46
+	 */
+	class LocationAdapter extends CommonAdapter<LocationInfo> {
+
+		public LocationAdapter(List<LocationInfo> list, Context context) {
+			super(list, context);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			LocationViewHolder holder = null;
+			if (convertView == null) {
+				holder = new LocationViewHolder();
+				convertView = inflater.inflate(R.layout.item_list_single_choice, parent, false);
+				
+				holder.checkedTextView = (CheckedTextView) convertView.findViewById(R.id.tv_content);
+				convertView.setTag(holder);
+			} else {
+				holder = (LocationViewHolder) convertView.getTag();
+			}
+			
+			final LocationInfo locationInfo = list.get(position);
+			
+			holder.checkedTextView.setText(locationInfo.getAddress());
+			
+			return convertView;
+		}
+		
+	}
+
+	final class LocationViewHolder {
+		CheckedTextView checkedTextView;
+	}
+
+	@Override
+	public void onSnapshotReady(Bitmap snapshot) {
+		if (snapshot != null) {
+			FileOutputStream fos = null;
+			try {
+				File file = new File(Environment.getExternalStorageDirectory(), System.currentTimeMillis() + ".jpg");
+				fos = new FileOutputStream(file);
+				snapshot.compress(CompressFormat.JPEG, 20, fos);
+				SystemUtil.makeShortToast("截图成功：" + file.getAbsolutePath());
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				if (fos != null) {
+					try {
+						fos.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				if (snapshot != null) {
+					snapshot.recycle();
+				}
+				
+			}
+		}
+	}
+}
